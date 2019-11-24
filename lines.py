33f9bd91a16844d59dabdf04fbe272a85d5fad7d
ugm2 <ugm2@alu.ua.es>
2019-11-24 20:23:11 +0100

    Creating exercises

---
+++ lines.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+import nengo
+
+D = 9
+cols = int(np.sqrt(D))
+size = 150
+
+model = nengo.Network()
+with model:
+    for i in range(D):
+        def waves(t, i=i):
+            return np.sin(t + np.arange(i + 1) * 2 * np.pi / (i + 1))
+        node = nengo.Node(waves)
+
+if __name__ == '__main__':
+    import nengo_gui
+    nengo_gui.GUI(__file__).start()
